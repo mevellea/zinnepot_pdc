7ab6ab5e1b0d763f34ab61c2b18bdff99eebbb5d
MeV <7838635+mevellea@users.noreply.github.com>
2026-04-06 18:25:44 +0200

    Filtre data

--- customers.py
+++ customers.py
@@ -6,27 +6,25 @@ TEMPLATE_FILE = "templates/template_cust.html"
 
 
 CUST_INPUT_FILE = r"H:\My Drive\3.PDC\Auto-cueillette Haren (Responses).xlsx"
+CUST_DATA_FILE = r"H:\My Drive\3.PDC\cueilleurs.xlsx"
 CUST_OUTPUT_FILE = r"H:\My Drive\3.PDC\export\clients.html"
 
 
 def load_data():
-    print(f" ### Loading customer file {CUST_INPUT_FILE}")
-    df = pd.read_excel(CUST_INPUT_FILE, skiprows=[1])
+    print(f" ### Loading file {CUST_INPUT_FILE}")
+    df = pd.read_excel(CUST_INPUT_FILE, skiprows=[1], index_col=11)
     df.drop(df.tail(2).index, inplace=True)  # drop last 2 rows
-
-    # Nettoyage colonnes
     df.columns = [c.strip() for c in df.columns]
 
-    # Supprimer colonnes adresse
-    df = df[[c for c in df.columns if "Adresse" not in c.lower()]]
-    df = df[[c for c in df.columns if "règles" not in c.lower()]]
     df = df.rename(columns={
         "Comment avez-vous entendu parler du projet d'auto-cueillette de l'Asbl Zinnepot ?": "Source",
         "Comment souhaitez-vous être informé des nouvelles du champ (récoltes disponibles, changement d'horaires, autres informations importantes...) ?": "Comm",
         "Souhaiteriez-vous avoir accès à d'autres produits ?": "Produits",
-        "En combien de fois voulez-vous payer l'abonnement?": "Echelonnement",
+        "En combien de fois voulez-vous payer l'abonnement?": "Paiement",
+        "Commentaires, allergies ou informations importantes à nous communiquer": "Commentaires",
         "Quels produits vous intéressent le plus ?": "Préférences",
         "Quel formule choisissez-vous ? 2": "Prix",
+        "Infos supplémentaires, si étudiants en kot précisez les âges": "Infos",
         "À quelle fréquence pensez-vous venir cueillir ?": "Frequence"
         }
     )
@@ -48,18 +46,24 @@ def load_data():
         elif row["Prix"] == "Prix soutient":
             raw_price_adulte = 280 / 1.06
 
-        raw_price_enfant = 13.0 * row["Total des âges des enfants"] / 1.06
-        if row["Echelonnement"] == "En deux fois, en début et milieu de saison":
+        raw_price_enfant = 13.0 * row["Ages des enfants"] / 1.06
+        if row["Paiement"] == "En deux fois, en début et milieu de saison":
             raw_price_adulte /= 2
             raw_price_enfant /= 2
+            payment = "Mi-saison"
+        else:
+            payment = "Complète"
 
-        df.iloc[index, df.columns.get_loc("Prix brut adulte x1")] = round(raw_price_adulte, 3)
-        df.iloc[index, df.columns.get_loc("Prix brut enfant tous")] = round(raw_price_enfant, 3)
+        df.loc[index, "Paiement"] = payment
+        df.loc[index, "Prix brut adulte x1"] = round(raw_price_adulte, 3)
+        df.loc[index, "Prix brut enfant tous"] = round(raw_price_enfant, 3)
 
     # Remplacer NaN
     df = df.fillna("")
 
-    return df
+    print(f" ### Loading data file {CUST_DATA_FILE}")
+    df_data = pd.read_excel(CUST_DATA_FILE, index_col=0)
+    return df.join(df_data)
 
 
 def split_fields(record):
@@ -69,12 +73,9 @@ def split_fields(record):
     for k, v in record.items():
         key = k.lower()
 
-        if k == "Nom et prénom":
-            continue
-
-        if any(x in key for x in ["date", "time"]):
+        if any(x in key for x in ["date", "time", "prénom", "règles", "objets"]):
             pass
-        if any(x in key for x in ["nombre", "prix", "adresse", "total", "kot", "echelonnement"]):
+        elif any(x in key for x in ["nombre", "prix", "adresse", "ages", "infos", "paiement"]):
             col1[k] = v
         else:
             col2[k] = v
@@ -84,15 +85,19 @@ def split_fields(record):
 
 def gen_customer_html():
     df = load_data()
-    items = df.to_dict(orient="records")
+    items = df.T.to_dict()
     processed = []
 
     for item in items:
-        col1, col2 = split_fields(item)
+        col1, col2 = split_fields(items[item])
+        notes = items[item]["Objets"]
+        if isinstance(notes, str):
+            notes = notes.split("\n")
         processed.append({
-            "name": item.get("Nom et prénom", ""),
+            "name": item,
             "col1": col1,
-            "col2": col2
+            "col2": col2,
+            "notes": notes
         })
 
     env = Environment(loader=FileSystemLoader("."))
@@ -101,4 +106,4 @@ def gen_customer_html():
 
     with open(CUST_OUTPUT_FILE, "w", encoding="utf-8") as f:
         f.write(html)
-    print("HTML customer generated :", CUST_OUTPUT_FILE)
+    print("HTML generated :", CUST_OUTPUT_FILE)
